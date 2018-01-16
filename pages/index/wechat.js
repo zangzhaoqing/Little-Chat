1514d6cdb810fc638d192f32f43e1db274edcec6
zangzhaoqing <472768416@qq.com>
2018-01-16 13:44:00 +0800

    new project

---
+++ pages/index/wechat.js
@@ -0,0 +1,47 @@
+Page({
+  onLoad:function(){
+    wx.checkIsSupportSoterAuthentication({
+      success(res) {
+        console.log(res);
+        // res.supportMode = [] 不具备任何被SOTER支持的生物识别方式
+        // res.supportMode = ['fingerPrint'] 只支持指纹识别
+        // res.supportMode = ['fingerPrint', 'facial'] 支持指纹识别和人脸识别
+      }
+    })
+  },  
+  bindViewTap:function(){
+    wx.chooseInvoiceTitle({
+      success(res) {
+        console.log(res)
+      }
+    })
+  },
+  getPhoneNumber:function(){
+    wx.getWeRunData({
+      success(res) {
+        console.log(res)
+        const encryptedData = res.encryptedData
+      },
+      fail(res){
+        console.log(res)
+      }
+    })
+  },
+  data: {
+    nodes: [{
+      name: 'div',
+      attrs: {
+        class: 'div_class',
+        style: 'line-height: 60px; color: red;text-align:center;'
+      },
+      children: [{
+        type: 'text',
+        text: 'Hello&nbsp;World!'
+      }]
+    }],
+    addTop:'addTop'
+  },
+  tap() {
+    console.log('tap')
+  }
+})
